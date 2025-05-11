d0026fbedce0e4cacc1af91b54916b1bdc40cc02
Code-Crafter <bipinbhandari737@gmail.com>
2025-05-11 18:59:54 +0545

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+alert("Hello World");
+let a=1;
+var b=22;
+{
+    let a=33;
+    console.log(a); // 33
+}
+{
+    var b=44;
+    console.log(b); 
+}
+console.log(a); // 1
+console.log(b); // 44console.log("hi");
